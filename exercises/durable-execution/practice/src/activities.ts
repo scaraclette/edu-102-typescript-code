dd8bdf6e1bc550d3a50d35d8574504ed47bf8212
Scarlett Halima <scarletthalima@secondspectrum.com>
2024-09-14 10:25:22 -0700

    durable-execution completed

--- exercises/durable-execution/practice/src/activities.ts
+++ exercises/durable-execution/practice/src/activities.ts
@@ -1,14 +1,17 @@
 import axios from 'axios';
 
 // TODO import everything from the @temporalio/activity package as activity
+import * as activity from '@temporalio/activity';
 
 import { TranslationActivityInput, TranslationActivityOutput } from './shared';
 
 export async function translateTerm(input: TranslationActivityInput): Promise<TranslationActivityOutput> {
   // TODO Define an Activity logger
+  const context = activity.Context.current();
 
   // TODO log Activity invocation, at the Info level, and include the term being
   //      translated and the language code as name-value pairs
+  context.log.info('Translating term:', { LanguageCode: input.languageCode, Term: input.term});
 
   const lang = encodeURIComponent(input.languageCode);
   const term = encodeURIComponent(input.term);
@@ -20,20 +23,24 @@ export async function translateTerm(input: TranslationActivityInput): Promise<Tr
     const content = response.data;
     // TODO  use the Debug log level to log the successful translation and include the
     //       translated term as a name-value pair
+    context.log.debug('Translation successful:', { translation: content });
     return { translation: content };
   } catch (error: any) {
     if (error.response) {
       // TODO  use the Error log level to log the failed response and include the
       //       response status and response data using error.response.status
       //       and error.response.data
+      context.log.error('Translation request failed:', { statue: error.response.statue, data: error.response.data });
       throw new Error(`HTTP Error ${error.response.status}: ${error.response.data}`);
     } else if (error.request) {
       // TODO  use the Error log level to log the failed request and include the
       //       value of error.request
+      context.log.error('Translation request failed:', { request: error.request });
       throw new Error(`Request error:  ${error.request}`);
     }
     // TODO  use the Error log level to log that something else went wrong with
     //       the translation. Print out the value of `error`.
+    context.log.error('Something else failed during translation', { error });
     throw new Error('Something else failed during translation.');
   }
 }
--- exercises/durable-execution/practice/src/workflows.ts
+++ exercises/durable-execution/practice/src/workflows.ts
@@ -1,5 +1,5 @@
 // TODO import the sleep and log packages here by adding it to the list.
-import { proxyActivities } from '@temporalio/workflow';
+import { proxyActivities, sleep, log } from '@temporalio/workflow';
 // Only import the activity types
 import type * as activities from './activities';
 import { TranslationWorkflowInput, TranslationWorkflowOutput } from './shared';
@@ -11,6 +11,8 @@ const { translateTerm } = proxyActivities<typeof activities>({
 export async function sayHelloGoodbyeWorkflow(input: TranslationWorkflowInput): Promise<TranslationWorkflowOutput> {
   // TODO Log, at the Info level, when the Workflow function is invoked
   //      and be sure to include the name passed as input
+  log.info('SayHelloGoodbye Workflow invoked', { Name: input.name });
+  log.debug('Preparing to translate hello', { LanguageCode: input.languageCode });
 
   const helloInput = {
     term: 'Hello',
@@ -19,10 +21,13 @@ export async function sayHelloGoodbyeWorkflow(input: TranslationWorkflowInput): 
 
   // TODO Log, at the Debug level, a message about the Activity to be executed,
   //      be sure to include the language code passed as input
+  log.debug('Preparing to translate goodbye', { LanguageCode: input.languageCode });
   const helloResult = await translateTerm(helloInput);
   const helloMessage = `${helloResult.translation}, ${input.name}`;
 
   // TODO: (Part C): log a message at the Debug level and then start a Timer for 10 seconds
+  log.info('Sleeping between translation calls', {});
+  await sleep('10 seconds');
 
   const goodbyeInput = {
     term: 'Goodbye',
